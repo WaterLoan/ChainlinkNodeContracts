85f3dd33320ded369272662f8bbfcb6612a580a2
LuckyKoala <pilotcraft.lk@gmail.com>
2022-03-11 16:47:03 +0800

    improvement: use hardhat task to simplify deployments

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # ChainLink 节点部署
 
+> oneswap 适配器是单独写的，从 OneSwap 市场 API 获取价格
+> 已经在正式服务器配置为 docker 环境
+
+## 部署
+
+> 每个币对需要一个 Aggregator，Oracle 地址就是 Chainlink 后端节点的对应地址，Admin 地址可以获取 Oracle 的报酬（如果报酬为 0 的，这个地址意义不大）
+> decimals 设置成跟 Chainlink 报价任务相同即可
+> 注意 min max 为报价最小最大值，超过的话链上合约不会接受
+
+* 部署 Chainlink 节点
+* 部署 Flags (默认报价偏离 5% 会 raiseFlag，可以搭配链下监控服务使用) `npx hardhat deployLibs --network <网络>`
+* 编辑 `hardhat.config.js` 配置网络 RPC 等信息
+* 部署单个 Aggregator `npx hardhat deployAggregator --admin 0xA4ff8Eb9F7331C2E93E60c5a2Cf5D694AC0f2811 --decimals 18 --description "ETH / USD" --max 100 --min 10 --oracle 0xA4ff8Eb9F7331C2E93E60c5a2Cf5D694AC0f2811 --link 0x5FbDB2315678afecb367f032d93F642f64180aa3 --validator 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9 --network <网络>`
+
 ## 基本合约
 
 * LinkToken 作为链下节点提供服务的奖励
---
+++ hardhat.config.js
@@ -0,0 +1,95 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require('@nomiclabs/hardhat-ethers');
+
+task(`deployLibs`, `Deploy Chainlink Libs`)
+    .setAction(
+        async () => {
+            const DeviationFlaggingValidator = await ethers.getContractFactory("DeviationFlaggingValidator");
+            const Flags = await ethers.getContractFactory("Flags");
+            const LinkToken = await ethers.getContractFactory("LinkToken");
+            const SimpleReadAccessController = await ethers.getContractFactory("SimpleReadAccessController");
+
+            const link = await LinkToken.deploy();
+            const rac = await SimpleReadAccessController.deploy();
+            const flags = await Flags.deploy(rac.address);
+            // Setting the value of 100,000 is equivalent to tolerating a 100%
+            const validator = await DeviationFlaggingValidator.deploy(flags.address, 5000);
+
+            console.log("LinkToken deployed to:", link.address);
+            console.log("SimpleReadAccessController deployed to:", rac.address);
+            console.log("Flags deployed to:", flags.address);
+            console.log("Validator deployed to:", validator.address);
+        }
+    );
+
+task(`deployAggregator`, `Deploy Chainlink Aggregator`)
+    .addParam("min", "lower bound of submission value")
+    .addParam("max", "upper bound of submission value")
+    .addParam("decimals", "decimals to offset the answer")
+    .addParam("description", "for example: ETH / USD")
+    .addParam("oracle", "the address which could submit answer")
+    .addParam("admin", "the address which could withdraw payment of according oracle")
+    .addParam("link", "the address of LinkToken")
+    .addParam("validator", "the address of DeviationFlaggingValidator")
+    .setAction(
+        async (args) => {
+            const FluxAggregator = await ethers.getContractFactory("FluxAggregator");
+            /**
+             * @notice set up the aggregator with initial configuration
+             * @param _link The address of the LINK token
+             * @param _paymentAmount The amount paid of LINK paid to each oracle per submission, in wei (units of 10⁻¹⁸ LINK)
+             * @param _timeout is the number of seconds after the previous round that are
+             * allowed to lapse before allowing an oracle to skip an unfinished round
+             * @param _validator is an optional contract address for validating
+             * external validation of answers
+             * @param _minSubmissionValue is an immutable check for a lower bound of what
+             * submission values are accepted from an oracle
+             * @param _maxSubmissionValue is an immutable check for an upper bound of what
+             * submission values are accepted from an oracle
+             * @param _decimals represents the number of decimals to offset the answer by
+             * @param _description a short description of what is being reported
+             */
+            // constructor(
+            //     address _link,
+            //     uint128 _paymentAmount,
+            //     uint32 _timeout,
+            //     address _validator,
+            //     int256 _minSubmissionValue,
+            //     int256 _maxSubmissionValue,
+            //     uint8 _decimals,
+            //     string memory _description
+            // )
+            const aggregator = await FluxAggregator.deploy(args.link, 0, 60, args.validator, args.min, args.max, args.decimals, args.description);
+            aggregator.changeOracles([], [args.oracle], [args.admin], 1, 1, 0);
+
+            console.log("Aggregator deployed to:", aggregator.address);
+        }
+    );
+
+module.exports = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.6.6",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000,
+                    },
+                },
+            },
+            {
+                version: "0.4.24",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000,
+                    },
+                },
+            },
+        ],
+    },
+};
